e5710e02a047b738e279fecb204da1133fa5aef1
SeekTheError <remi.bouchar@gmail.com>
2011-11-18 20:55:31 +0100

    a lot

--- server.js
+++ server.js
@@ -5,46 +5,144 @@ var port= 1337;
 var listenTo = "127.0.0.1";
 
 //route configuration
-Dispatcher= function () {return "Hello World on HP";}
+Dispatcher = {}
 
-Dispatcher["hello"]= function () {var response = "Hello World, acces url was "+this.req.url;return response;}
+var path = require('path');
+var fs = require('fs');
+Dispatcher["/static/.*"]= function () {
+  var filePath = '.' + this.requestItems.path;
+  var scope = this;
+  console.log("file path: "+filePath);
+    path.exists(filePath, function(exists) {
+    if (exists) {
+            fs.readFile(filePath, function(error, content) {
+                if (error) {
+                    scope.res.writeHead(500);
+                    scope.res.end();
+                }
+                else {
+                    scope.res.writeHead(200);
+                    scope.res.end(content, 'utf-8');
+                }
+            });
+        }
+        else {
+  	    _404.apply(scope);
+            //scope.res.writeHead(404, { 'Content-Type': 'text/html' });
+            //scope.res.end("File Not Found", 'utf-8');
+        }
+});
+}
+Dispatcher["index.html"]= function () {_wsc(this,200,"text/plain");_we(this,"Hello World on HP");}
 
-Dispatcher["hello"]["tcm"] = function() {return "Hello tcm";}
+_404 =  function () {_wsc(this,404,  'text/plain');_we(this,"404, Page not found");}
+
+Dispatcher["/favicon.ico"] = function () { this.requestItems.path = "/static/favicon.ico"; Dispatcher["/static/.*"].apply(this);  }
+
+Dispatcher["_404"]= _404;
+
+
+
+//server
 
 var http = require('http');
 http.Dispatcher=Dispatcher;
 http.createServer(function (req, res) {
   //the content response
   var content;
-  var param = req.url.split("/");
-  var route =Array();
-  console.log("url:"+req.url);
-  for (var p in param){if(param[p] != "") {route.push(param[p]);console.log(p);}}
-    
-  console.log("target length:"+route.length);
+  var requestItems=parseUri(req.url);
+  var requestPath=Utils.stripTrailingSlash(requestItems.path);
+  console.log("request:"+requestPath);
   var fToCall;
-  if (route.length == 0){
-  fToCall = Dispatcher;
-    }
-  else if (route.length == 1 ){
-  fToCall = Dispatcher[route[0]];
-    }
-  else if (route.length == 2 ) {
-  fToCall = Dispatcher[route[0]][route[1]];
-    }
+  for (route in Dispatcher) {
+  if( requestPath == ""){fToCall = Dispatcher["index.html"]}
+  console.log("trying route: "+route);
+  if (requestPath.match(route)){
+    fToCall = Dispatcher[route];
+    console.log("match, using route: "+route);
+    break;
+  }
+ }
   if( typeof fToCall === "undefined"){
-   res.writeHead(404, {'Content-Type': 'text/plain'});
-   res.end("404, Page not found");
-  }else {
+  fToCall= Dispatcher["_404"] ;
+  }
+  Utils.createScope(res,req,requestItems);
+  content = fToCall.apply(scope);
+}).listen(port,listenTo);
+console.log("server running at "+listenTo+":"+port);
+
+////////////////////////////////////////////////
+//UTILS part
+
+Constant = {}
+Constant.contentType= {}
+Constant.contentType.plainText = 'text/plain';
+
+Utils = {}
+
+Utils.stripTrailingSlash = function (str) {
+    if(str.substr(-1) == '/') {
+        return str.substr(0, str.length - 1);
+    }
+    return str;
+}
+
+
+Utils.createScope= function (res,req,requestItems){
   scope={}
   scope.res=res;
   scope.req=req;
-  content = fToCall.apply(scope);
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  console.log(content);
-  res.end(content);}
-}).listen(port,listenTo);
-console.log("server running at "+listenTo+":"+port);
+  scope.requestItems = requestItems;
+  return scope;
+}
+
+Utils.writeStatusContentType = function (scope,status,contentType){
+ scope.res.writeHead(status, {'Content-Type': contentType});
+}
+Utils.writeEnd = function(scope,content){
+scope.res.end(content);
+}
+_wsc = Utils.writeStatusContentType ;
+_we  = Utils.writeEnd;
+
+///////////////////////
+// LIBS
+
+// parseUri 1.2.2
+// (c) Steven Levithan <stevenlevithan.com>
+// MIT License
+
+function parseUri (str) {
+	var	o   = parseUri.options,
+		m   = o.parser[o.strictMode ? "strict" : "loose"].exec(str),
+		uri = {},
+		i   = 14;
+
+	while (i--) uri[o.key[i]] = m[i] || "";
+
+	uri[o.q.name] = {};
+	uri[o.key[12]].replace(o.q.parser, function ($0, $1, $2) {
+		if ($1) uri[o.q.name][$1] = $2;
+	});
+
+	return uri;
+};
+
+parseUri.options = {
+	strictMode: false,
+	key: ["source","protocol","authority","userInfo","user","password","host","port","relative","path","directory","file","query","anchor"],
+	q:   {
+		name:   "queryKey",
+		parser: /(?:^|&)([^&=]*)=?([^&]*)/g
+	},
+	parser: {
+		strict: /^(?:([^:\/?#]+):)?(?:\/\/((?:(([^:@]*)(?::([^:@]*))?)?@)?([^:\/?#]*)(?::(\d*))?))?((((?:[^?#\/]*\/)*)([^?#]*))(?:\?([^#]*))?(?:#(.*))?)/,
+		loose:  /^(?:(?![^:@]+:[^:@\/]*@)([^:\/?#.]+):)?(?:\/\/)?((?:(([^:@]*)(?::([^:@]*))?)?@)?([^:\/?#]*)(?::(\d*))?)(((\/(?:[^?#](?![^?#\/]*\.[^?#\/.]+(?:[?#]|$)))*\/?)?([^?#\/]*))(?:\?([^#]*))?(?:#(.*))?)/
+	}
+};
+
+// 
+
 
 
 
